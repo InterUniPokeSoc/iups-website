eca50476792cd6df340efcdee9638f2dee73419c
TheMattSykes <matt.sykes@hotmail.co.uk>
2021-07-02 15:12:03 +0100

    Module update

---
+++ .cache/sync-requires.js
@@ -0,0 +1,11 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Volumes/Matt-SSD-Store/Projects/iups-website/.cache/dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("/Volumes/Matt-SSD-Store/Projects/iups-website/src/pages/404.js")),
+  "component---src-pages-index-js": preferDefault(require("/Volumes/Matt-SSD-Store/Projects/iups-website/src/pages/index.js"))
+}
+
