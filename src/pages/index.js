8992732e210c345f4b8048ae4ae577c158db8737
TheMattSykes <matt.sykes@hotmail.co.uk>
2021-08-01 22:46:02 +0100

    Homepage redesign with background image.

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,26 @@ import RoundButton from '../components/roundButton';
 function IndexPage() {
   return (
     <Layout>
-      <main className="main-area-flex">
+      <main className="main-content">
+        <img className="main-bg" src="/images/sheffieldbg.jpeg" />
+        <div className="main-area-flex">
+          <div className="info">
+            <h1 className="index-title title">A Nationwide Group of Pokémon Societies</h1>
+            <p>
+            The Inter-University Pokémon Society is a union of more than 30 societies joined together to collaborate on all things Pokémon.
+            </p>
+            <p>
+            Our primary goal to unite and help form Pokémon societies across the country, if you're looking to set a PokéSoc up, we can help you out!
+            </p>
+          </div>
+          <div id="home-logo-wrapper">
+            <img id="home-logo" src="/images/doggo.png" />
+          </div>
+        </div>
+      </main>
+
+
+      {/* <main className="main-area-flex">
         <div className="info">
           <h1 className="index-title title primary-color">A Nationwide Group of Pokémon Societies</h1>
           <p>
@@ -23,15 +42,15 @@ function IndexPage() {
         <div id="home-logo-wrapper">
           <img id="home-logo" src="/images/doggo.png" />
         </div>
-      </main>
+      </main> */}
 
-      <section className="map-link-section">
+      {/* <section className="map-link-section">
         <img className="map-link-background" src="/images/sheffield.jpg" alt="City scape of Sheffield during sunset with central focus on the Arts Tower." />
         <div className="map-link-info">
           <p className="title map-link-title">Our Societies</p>
           <RoundButton className="map-link-button" link="./our-societies" text="Show Me" />
         </div>
-      </section>
+      </section> */}
 
     </Layout>
   );
