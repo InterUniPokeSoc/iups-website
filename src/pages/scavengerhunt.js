95d64482983cca10735e533fff90234c31a617f5
TheMattSykes <matt.sykes@hotmail.co.uk>
2021-09-06 01:14:41 +0100

    Mostly working scavenger hunt with db connection.

--- src/pages/scavengerhunt.js
+++ src/pages/scavengerhunt.js
@@ -8,58 +8,77 @@ import '../styles/scavengerHunt.scss';
 
 import {getHints} from "../utils/hints";
 
-class Hint {
-  constructor (answer, hint) {
-    this.answer = answer;
-    this.hint = hint;
-  }
-}
-
 function ScavengerHuntPage() {
 
-  var hintsList = getHints;
-  console.log(hintsList);
-
-  // console.log(hintsList);
-
-  let hints = [
-    "What is the best Pokémon?",
-    "Aquire the?",
-    "That's very...",
+  var hints = [
+    // "What is the best Pokémon?",
+    // "Aquire the?",
+    // "That's very...",
   ];
 
-  let answers = [
-    "Quag",
-    "Sire",
-    "Sus",
+  var answers = [
+    // "Quag",
+    // "Sire",
+    // "Sus",
   ];
+
+  var list = getHints().then((dbList) => {
+    console.log(dbList[0].hint1.hint);
+
+    Object.values(dbList[0]).map((hint) => {
+      hints.push(hint.hint);
+      answers.push(hint.answer);
+    });
+
+    generateHint();
+  });
 
   var currentHintNo = 0;
 
+  function generateHint() {
+    var sectionDiv = document.getElementById("hints-page");
+    var hintDiv = document.createElement('div');
+    hintDiv.classList = ["hint-wrapper"];
+
+    var hintTitle = document.createElement('h2');
+    hintTitle.classList = ["hint-title"];
+    hintTitle.innerHTML = "Hint " + (currentHintNo+1);
+
+    var hintText = document.createElement('p');
+    hintText.classList = ["hint-text"];
+    hintText.id = "hint-text-"+currentHintNo;
+    hintText.innerHTML = hints[currentHintNo];
+
+    var hintInput = document.createElement('input');
+    hintInput.classList = ["hint-input"];
+    hintInput.id = "hint-input-"+currentHintNo;
+
+    hintDiv.appendChild(hintTitle);
+    hintDiv.appendChild(hintText);
+    hintDiv.appendChild(hintInput);
+
+    sectionDiv.appendChild(hintDiv);
+  }
+
+
   let questionResponse = () => {
-    var userAnswer = document.getElementById("question-" + currentHintNo + "-input").value;
+    var userAnswer = document.getElementById("hint-input-"+currentHintNo).value;
     var updateMessageBox = document.getElementById("update-message");
 
     console.log(userAnswer + "     " + answers[currentHintNo]);
 
     if (userAnswer == answers[currentHintNo]) {
       console.log("Answer is Correct!");
-      updateMessageBox.innerHTML = "Correct! The next hint has be revealed to you.";
+      updateMessageBox.innerHTML = "Correct! The next hint has been revealed to you.";
 
       // Disable old inputs
-      document.getElementById("question-" + currentHintNo + "-button").disabled = true;
+      document.getElementById("hint-input-"+currentHintNo).disabled = true;
 
+      // Increment hint number
       currentHintNo++;
       
       if (currentHintNo < hints.length) {
-
-        // Set new text - previously set to standard text to prevent cheating
-        document.getElementById("question-" + currentHintNo + "-text").innerHTML = hints[currentHintNo];
-
-        // Show the new hint and inputs
-        document.getElementById("hint-wrapper-" + currentHintNo).classList.remove("hide");
-        document.getElementById("hint-wrapper-" + currentHintNo).classList.add("show");
-        
+        generateHint();
       } else {
         console.log("Congrats you've won the prize!");
         updateMessageBox.innerHTML = "Congrats, you've won the Scavenger Hunt! If you were first you can claim the prize!";
@@ -81,20 +100,12 @@ function ScavengerHuntPage() {
       <main className="page-content">
         <TitleBanner imageSource="" title="Scavenger Hunt" />
 
-        <section className="page-section">
+        <section className="page-section" id="hints-page">
+        </section>
 
-          { hints.map((value, index) => (
-            <div key={"hint-wrapper-"+index} id={"hint-wrapper-"+index} className={"hint-wrapper " + (index > 0 ? "hide" : "show")}>
-              <h1 className="title">Hint {index+1}</h1>
-              <p id={"question-" + index + "-text"}>{index == 0 ? value : "Nice try! The browser inspector isn't going to help you this time!"}</p>
-              <input id={"question-" + index + "-input"} type="text"></input>
-              <label id={"question-" + index + "-label"}></label>
-              <button id={"question-" + index + "-button"} onClick={questionResponse}>Check Answer</button>
-            </div>
-          ))}
-
+        <section className="page-section" id="info-section">
+          <a id={"hint-button"} onClick={questionResponse}>Check Answer</a>
           <p id="update-message"></p>
-
         </section>
 
       </main>
--- src/utils/hints.js
+++ src/utils/hints.js
@@ -1,16 +1,42 @@
+import React from "react";
 import {db} from "./firebase";
 import { collection, getDocs } from 'firebase/firestore/lite';
 import firestore from 'firebase/firestore';
+import { FirebaseError } from "firebase/app";
 
+// class Hint {
+//   constructor (answer, hint) {
+//     this.answer = answer;
+//     this.hint = hint;
+//   }
+// }
 
 async function getHints() {
   const hintsCol = collection(db, 'scavenger-hunt');
   const hintSnapshot = await getDocs(hintsCol);
-  const hintsList = hintSnapshot.docs.map(doc => ({
-    id: doc.id,
-    ... doc.data()
-  }));
+  var hintsList = hintSnapshot.docs.map(function(doc) {
+    return doc.data();
+  });
   return hintsList;
 }
+
+// export const useGetData = () => {
+//   const [documents, setDocuments] = React.useState([]);
+//   const db = firebase.firestore();
+
+//   React.useEffect(() => {
+//     db.collection("scavenger-hunt")
+//       .get()
+//       .then((querySnapshot) => {
+//         let arr = [];
+//         querySnapshot.docs.map((doc) =>
+//           arr.push({ id: doc.id, value: doc.data() })
+//         );
+//         setDocuments(arr);
+//       })
+//   }, [db]);
+
+//   return documents;
+// }
 
 export { getHints };
