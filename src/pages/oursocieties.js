0421a15d1e97ed6eb6fb3d6fba38a07d61e873a8
TheMattSykes <matt.sykes@hotmail.co.uk>
2021-12-18 23:17:56 +0000

    Society list on Our Societies is now dynamic and objects stored instead of 2D array.

--- src/pages/oursocieties.js
+++ src/pages/oursocieties.js
@@ -8,24 +8,111 @@ import Map from '../components/map';
 
 import { getSocieties } from "../utils/societies";
 
+var societies = [];
+
+function createSocietySidebarItem(id) {
+  console.log("createSocietySidebarItem() called for "+id);
+  console.log("Data: "+societies[id]);
+  var sideBar = document.getElementById('sidebar');
+
+  var sideBarItem = document.createElement('div');
+  sideBarItem.classList.add('society-wrapper');
+
+  var societyName = document.createElement('h2');
+  societyName.classList.add('society-name');
+  societyName.innerHTML = societies[id].name;
+  sideBarItem.appendChild(societyName);
+
+  var color1 = societies[id].color1;
+  var color2 = societies[id].color1;
+
+  if (color1 != null) {
+    sideBarItem.style.backgroundColor = "#"+color1;
+  }
+
+  if (color1 != null && color2 != null) {
+    sideBarItem.style.backgroundColor = "linear-gradient(#"+color1+",#"+color2+")";
+  }
+
+
+  // Social Media Icons
+  var fbIconWrapper = document.createElement('a');
+  var instIconWrapper = document.createElement('a');
+  var discIconWrapper = document.createElement('a');
+
+  fbIconWrapper.classList.add('social-icon-society-wrapper');
+  instIconWrapper.classList.add('social-icon-society-wrapper');
+  discIconWrapper.classList.add('social-icon-society-wrapper');
+
+  var fbIcon = document.createElement('img');
+  var instIcon = document.createElement('img');
+  var discIcon = document.createElement('img');
+
+  fbIcon.classList.add('social-icon-society');
+  instIcon.classList.add('social-icon-society');
+  instIcon.classList.add('invert-icon');
+  discIcon.classList.add('social-icon-society');
+
+  fbIcon.src = "/images/f_logo_RGB-White_250.png";
+  instIcon.src = "/images/instagram-logo-black.png";
+  discIcon.src = "/images/icon_clyde_white_RGB.svg";
+
+  var fbLink = societies[id].facebook;
+  var instLink = societies[id].instagram;
+  var discLink = societies[id].discord;
+
+  if (fbLink != null) {
+    fbIconWrapper.href = fbLink;
+    fbIconWrapper.appendChild(fbIcon);
+    sideBarItem.appendChild(fbIconWrapper);
+  }
+
+  if (instLink != null) {
+    instIconWrapper.href = instLink;
+    instIconWrapper.appendChild(instIcon);
+    sideBarItem.appendChild(instIconWrapper);
+  }
+
+  if (discLink != null) {
+    discIconWrapper.href = discLink;
+    discIconWrapper.appendChild(discIcon);
+    sideBarItem.appendChild(discIconWrapper);
+  }
+
+  // Add to Sidebar
+  sideBar.appendChild(sideBarItem);
+}
+
+function addSocietiesToSidebar() {
+  var sideBar = document.getElementById('sidebar');
+
+  // Remove all society elements from sidebar in case page reload duplicates items.
+  while (sideBar.lastChild) {
+    if (sideBar.firstChild === sideBar.lastChild) {
+      break;
+    } else {
+      sideBar.lastChild.remove()
+    }
+  }
+
+  // Add each society as sidebar item
+  societies.forEach(function (_, index) {
+    createSocietySidebarItem(index)
+  });
+}
+
+
 function OurSocietiesPage() {
 
-  var societies = [];
-
   getSocieties().then((dbList) => {
-    // console.log(dbList[0].hint1.hint);
-    console.log(dbList);
     Object.values(dbList).map((society) => {
-
-      societies.push([
-        society.name,
-        society.longitude,
-        society.latitude
-      ]);
+      societies.push(society);
     });
 
-  }).catch((e) => {
+    addSocietiesToSidebar()
 
+  }).catch((e) => {
+    console.log(e);
   });
 
   return (
@@ -41,20 +128,8 @@ function OurSocietiesPage() {
             <Map />
           </div>
 
-          <section className="sidebar">
-
+          <section className="sidebar" id="sidebar">
             <input className="search-bar" placeholder="Search for a society..."></input>
-
-            <div className="society-wrapper">
-              {/* {societies.map(society => (
-                <div>
-                  <h2 className="society-name">{society.name}</h2>
-                  <p className="society-name">{society.longitude}</p>
-                  <p className="society-name">{society.latitude}</p>
-                </div>
-              ))} */}
-              <h2 className="society-name">Sheffield PokéSoc</h2>
-            </div>
           </section>
         </div>
       </main>
