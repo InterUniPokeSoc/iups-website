93dca9a88f52a6ac4ef8fc84ef489d7c0bc9f640
Matt Sykes <matt.sykes@hotmail.co.uk>
2022-07-31 00:04:52 +0100

    Continuing development of about page and some refactoring.

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,9 +2,8 @@ import React from 'react';
 import {Link, useState, useRef, useEffect} from 'gatsby';
 import {Helmet} from "react-helmet";
 import Layout from '../components/layout';
-import TitleBanner from '../components/titleBanner';
 import '../styles/general.scss';
-import '../styles/about.scss';
+import * as aboutStyles from '../styles/about.module.scss';
 import CircleImageTitle from '../components/circleImageTitle';
 
 function AboutPage() {
@@ -17,18 +16,50 @@ function AboutPage() {
 
     <Layout>
       <main className="page-content">
-        <section className="page-section">
-
+        <section>
           <CircleImageTitle src="images/harold-wide.jpg">About Us</CircleImageTitle>
 
-          {/* <img src="images/harold-wide.jpg" id="about-image"/> */}
-          <h1 className="htj-title">What is the I-UPS?</h1>
-          <p>The Inter-University Pokémon Society is a group of over 20 university countries around the UK, from Durham to Portsmouth.
+          <h1 className="shiny-title center-margins medium-title">What is the I-UPS?</h1>
+          <p className={aboutStyles.paragraph}>The Inter-University Pokémon Society is a group of over 20 university countries around the UK, from Durham to Portsmouth.
             The I-UPS is designed to give university societies a central space to communicate and organise with each other, we also provide
             assistance to people looking to set up new university Pokémon societies.
           </p>
 
-          <h2 className="htj-title">Further Information</h2>
+          <section className={aboutStyles.iconsContainer}>
+            <div className={aboutStyles.icon}>
+              <div className={aboutStyles.iconImageContainer}>
+                <div className={aboutStyles.iconImageBackground}>
+                  <img className={aboutStyles.iconImage} />
+                </div>
+              </div>
+              <a className={aboutStyles.iconText}>Community of 20+ Societies</a>
+            </div>
+
+            <div className={aboutStyles.icon}>
+              <div className={aboutStyles.iconImageContainer}>
+                <div className={aboutStyles.iconImageBackground}>
+                  <img className={aboutStyles.iconImage} />
+                </div>
+              </div>
+              <a className={aboutStyles.iconText}>Multi-Uni Events</a>
+            </div>
+
+            <div className={aboutStyles.icon}>
+              <div className={aboutStyles.iconImageContainer}>
+                <div className={aboutStyles.iconImageBackground}>
+                  <img className={aboutStyles.iconImage} />
+                </div>
+              </div>
+              <a className={aboutStyles.iconText}>Discord Server</a>
+            </div>
+          </section>
+
+          <div className={aboutStyles.wideOverlayImageContainer}>
+            <a className={aboutStyles.hoverBox}>A Pokémon Community</a>
+            <img className={aboutStyles.wideOverlayImage} src="images/londongang.png"/>
+          </div>
+
+          <h2 className="shiny-title center-margins medium-title">Further Information</h2>
           <ul>
             <li>Pokémon and All Respective Names are Trademark & &copy; of Nintendo 1996-2022.</li>
           </ul>
