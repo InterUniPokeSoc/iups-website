0d1d63bbb1430c4e6c4d10766de0ddb3e72dcb55
Matt Sykes <matt.sykes@hotmail.co.uk>
2022-07-31 16:33:35 +0100

    Attempted Fix: Window error during build.

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,3 +26,17 @@ module.exports = {
     },
   ],
 };
+
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === "build-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            use: loaders.null(),
+          },
+        ],
+      },
+    })
+  }
+}
