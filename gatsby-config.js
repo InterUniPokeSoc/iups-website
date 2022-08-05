cc92db58f429fe521517ff25ff73bdcdb19b997d
Matt Sykes <matt.sykes@hotmail.co.uk>
2022-08-05 23:02:03 +0100

    Attempted fix 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,17 +28,17 @@ module.exports = {
 };
 
 exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
-  if (stage === 'build-html') {
+  if (stage === "build-html" || stage === "develop-html") {
     actions.setWebpackConfig({
       module: {
         rules: [
           {
             test: /react-leaflet|leaflet/,
             use: loaders.null(),
-          },
-        ],
-      },
+          }
+        ]
+      }
     })
   }
-}
+};
 
