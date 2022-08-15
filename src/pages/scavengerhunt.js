8aa8ace7fc726b5401d027b92d94cf2ceebb90f8
Matt Sykes <matt.sykes@hotmail.co.uk>
2022-08-15 20:51:57 +0100

    Refactor Scavenger Hunt code to use state changes.

--- src/pages/scavengerhunt.js
+++ src/pages/scavengerhunt.js
@@ -1,8 +1,6 @@
-import React from 'react';
-import {Link, useState, useRef, useEffect} from 'gatsby';
+import React, { useEffect, useState, useRef } from 'react'
 import {Helmet} from "react-helmet";
 import Layout from '../components/layout';
-import TitleBanner from '../components/titleBanner';
 import '../styles/general.scss';
 import '../styles/scavengerHunt.scss';
 
@@ -10,88 +8,54 @@ import {getHints, getWinners} from "../utils/hints";
 
 function ScavengerHuntPage() {
 
-  var hints = []
+  // List of Hints
+  const [hints, setHints] = useState([])
 
+  // Current Hint Number
+  const [currentHintNo, setCurrentHintNo] = useState(0)
+
+  // User Input
+  const [userAnswer, setUserAnswer] = useState(null)
+
+  // Output Messages
+  const [message, setMessage] = useState("Loading...")
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  // Get Hints from the database and store in hints
   getHints().then((dbList) => {
+    var tempHintList = []
+
     Object.values(dbList).map((hint) => {
-      hints.push(hint);
+      tempHintList.push(hint);
     });
 
-    document.getElementById("update-message").innerHTML = "";
-    document.getElementById("hint-button").classList.remove("hide");
-
-    generateHint();
+    setHints(tempHintList)
+    setMessage("")
   }).catch((e) => {
-    if (e.contains("available")) {
-      document.getElementById("update-message").innerHTML = "There is no Scavenger Hunt currently available.";
+    if (e.message.includes("available")) {
+      setErrorMessage("There is no Scavenger Hunt currently available.")
     } else {
-      document.getElementById("update-message").innerHTML = "An error occurred while retrieving the scavenger hunt data. Please contact a member of Comté.";
+      setErrorMessage("An error occurred while retrieving the scavenger hunt data. Please contact a member of Comté.")
     }
-
-    document.getElementById("update-message").style.fontSize = "1.4em";
-    document.getElementById("update-message").style.fontWeight = "bold";
-    document.getElementById("update-message").style.color = "red";
-    console.log(e);
   });
 
-  var currentHintNo = 0;
-
-  function generateHint() {
-    var sectionDiv = document.getElementById("hints-page");
-    var hintDiv = document.createElement('div');
-    hintDiv.classList = ["hint-wrapper"];
-
-    var hintTitle = document.createElement('h2');
-    hintTitle.classList = ["hint-title"];
-    hintTitle.innerHTML = "Hint " + (currentHintNo+1);
-
-    var hintText = document.createElement('p');
-    hintText.classList = ["hint-text"];
-    hintText.id = "hint-text-"+currentHintNo;
-    hintText.innerHTML = hints[currentHintNo].hint;
-
-    var hintInput = document.createElement('input');
-    hintInput.classList = ["hint-input"];
-    hintInput.id = "hint-input-"+currentHintNo;
-    hintInput.onchange = (() => { 
-      hintInput.classList.remove("error"); 
-      document.getElementById("update-message").innerHTML = "";
-    });
-
-    hintDiv.appendChild(hintTitle);
-    hintDiv.appendChild(hintText);
-    hintDiv.appendChild(hintInput);
-
-    sectionDiv.appendChild(hintDiv);
-  }
-
-
+  // Manage user input
   let questionResponse = () => {
-    var userInputBox = document.getElementById("hint-input-"+currentHintNo);
-    var userAnswer = document.getElementById("hint-input-"+currentHintNo).value;
-    var updateMessageBox = document.getElementById("update-message");
+    var userInputBox = document.getElementById("hint-input");
 
     if (hints[currentHintNo].answers.includes(userAnswer.toLowerCase())) {
-      updateMessageBox.innerHTML = "Correct! The next hint has been revealed to you.";
-
-      // Disable old inputs
-      document.getElementById("hint-input-"+currentHintNo).disabled = true;
+      setMessage("Correct! The next hint has been revealed to you.")
 
       // Increment hint number
-      currentHintNo++;
+      setCurrentHintNo(currentHintNo + 1)
       
-      if (currentHintNo < hints.length) {
-        generateHint();
-      } else {
-        console.log("Congrats you've won the prize!");
-        updateMessageBox.innerHTML = "Congrats, you've won the Scavenger Hunt! If you were first you can claim the prize!";
+      if (currentHintNo >= hints.length) {
+        setMessage("Congrats, you've won the Scavenger Hunt! If you were first you can claim the prize!")
       }
 
     } else {
       userInputBox.classList.add("error");
-
-      console.log("Answer is Incorrect!");
-      updateMessageBox.innerHTML = "Try again!";
+      setMessage("Try again!")
     }
   };
 
@@ -103,14 +67,20 @@ function ScavengerHuntPage() {
 
     <Layout>
       <main className="page-content">
-        <TitleBanner imageSource="images/pelipperbg.jpg" title="Scavenger Hunt" />
 
-        <section className="page-section" id="hints-page">
-        </section>
+        { hints.length > 0 &&
+          <section className="page-section hint-wrapper">
+            <h2 id="hint-title">Hint { currentHintNo + 1 }</h2>
+            <p id="hint-text">{ hints[currentHintNo].hint }</p>
+            <input id="hint-input" value={ userAnswer } onInput={ e => setUserAnswer(e.target.value) }></input>
+            <a id={"hint-button"} onClick={questionResponse}>Check Answer</a>
+          </section>
+        }
 
         <section className="page-section" id="info-section">
-          <a id={"hint-button"} className="hide" onClick={questionResponse}>Check Answer</a>
-          <p id="update-message">Loading...</p>
+          <p id="update-message" className={ errorMessage != null ? "error-message" : "" }>
+            { errorMessage == null ? message : errorMessage }
+          </p>
         </section>
 
       </main>
