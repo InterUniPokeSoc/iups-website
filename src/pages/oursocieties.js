b8a536ff561baddf1d80cd8ab2b1e1a3668d778e
Matt Sykes <matt.sykes@hotmail.co.uk>
2022-08-17 22:15:41 +0100

    Add twitter to society bubbles and fix insta logo hover issue.

--- src/pages/oursocieties.js
+++ src/pages/oursocieties.js
@@ -8,17 +8,20 @@ import Map from '../components/map';
 import { getSocieties } from "../utils/societies";
 
 // Image Imports
-import InstaIcon from '../images/social-media-icons/instagram/instagram-logo-black.png'
+import InstaIcon from '../images/social-media-icons/instagram/instagram-logo-white.png'
 import DiscordIcon from '../images/social-media-icons/discord/icon_clyde_white_RGB.svg'
 import FacebookIcon from '../images/social-media-icons/facebook/f_logo_RGB-White_250.png'
+import TwitterIcon from '../images/social-media-icons/twitter/twitter-logo-white.svg'
 import UpIcon from '../images/icons/up-arrow.svg'
 import DownIcon from '../images/icons/down-arrow.svg'
 
 
 export default function OurSocietiesPage() {
 
+  // List containing all societies retrieved from the database, should only be set once
   const [fullSocietiesList, setFullSocietiesList] = useState([])
 
+  // List containing societies filtered based on search query
   const [societies, setSocieties] = useState([])
 
   const [selectedSociety, setSelectedSociety] = useState(null)
@@ -111,24 +114,30 @@ export default function OurSocietiesPage() {
                   }
                   >
                     {/* Society Name Title */}
-                    <h2 className={ 'society-name' }> {society.name} </h2>
+                    <h2 className={ 'society-name' }> { society.name ?? "" } </h2>
 
                     {/* Social Media Links */}
                     { society.discord != null &&
-                      <a className={ 'social-icon-society-wrapper' } href={ society.discord }>
+                      <a className={ 'social-icon-society-wrapper' } href={ society.discord } target={ '_blank' } rel={ 'noopener noreferrer' }>
                         <img className={ 'social-icon-society' } src={ DiscordIcon } alt={ "Discord Icon" }/>
                       </a>
                     }
 
                     { society.instagram != null &&
-                      <a className={ 'social-icon-society-wrapper' } href={ society.instagram }>
-                        <img className={ ['social-icon-society', 'invert-icon'].join(' ') } src={ InstaIcon } alt={ "Instagram Icon" }/>
+                      <a className={ 'social-icon-society-wrapper' } href={ society.instagram } target={ '_blank' } rel={ 'noopener noreferrer' }>
+                        <img className={ 'social-icon-society' } src={ InstaIcon } alt={ "Instagram Icon" }/>
                       </a>
                     }
 
                     { society.facebook != null &&
-                      <a className={ 'social-icon-society-wrapper' } href={ society.facebook }>
+                      <a className={ 'social-icon-society-wrapper' } href={ society.facebook } target={ '_blank' } rel={ 'noopener noreferrer' }>
                         <img className={ 'social-icon-society' } src={ FacebookIcon } alt={ "Facebook Icon" }/>
+                      </a>
+                    }
+
+                    { society.twitter != null &&
+                      <a className={ 'social-icon-society-wrapper' } href={ society.twitter } target={ '_blank' } rel={ 'noopener noreferrer' }>
+                        <img className={ 'social-icon-society' } src={ TwitterIcon } alt={ "Twitter Icon" }/>
                       </a>
                     }
                   </a>
