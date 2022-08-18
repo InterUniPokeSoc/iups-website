a75d22aa71cdf575e843825f8a7e01ab6527a2c8
Matt Sykes <matt.sykes@hotmail.co.uk>
2022-08-18 23:01:51 +0100

    Moved index styles to module and made bg more efficient.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import {Link} from 'gatsby';
-import {Helmet} from "react-helmet";
+import { Helmet } from "react-helmet";
 import Layout from '../components/layout';
-import roundButton from '../components/roundButton';
-import '../styles/general.scss';
-import '../styles/index.scss';
+
+// Component Imports
 import RoundButton from '../components/roundButton';
-import TitleBanner from '../components/titleBanner';
 import VideoBanner from '../components/videoBanner';
 
+// Styles Imports
+import '../styles/general.scss';
+import * as styles from '../styles/index.module.scss';
+
 // Image Imports
-import Background from '../images/backgrounds/sheffieldbg.jpeg'
+import Background from '../images/backgrounds/sheffieldbg.jpg'
 import Doggo from '../images/branding/doggo.png'
 import InstaIcon from '../images/social-media-icons/instagram/instagram-logo-black.png'
 import DiscordIcon from '../images/social-media-icons/discord/full_logo_blurple_RGB.svg'
@@ -30,10 +31,10 @@ function IndexPage() {
     <Helmet>
       <title>Inter-Uni PokéSoc</title>
     </Helmet>
-      <main className="main-content">
-        <img className="main-bg" src={ Background } alt="Night time image of Sheffield used as background." />
-        <div className="main-area-flex">
-          <div className="info">
+      <main className={ styles.mainContent }>
+        <img className={ styles.mainBg } src={ Background } alt="Night time image of Sheffield used as background." />
+        <div className={ styles.mainAreaFlex }>
+          <div className={ styles.info }>
             <h1 className="medium-title shiny-title">A Nationwide Group of Pokémon Societies</h1>
             <p>
             The Inter-University Pokémon Society is a union of more than 30 societies joined together to collaborate on all things Pokémon.
@@ -42,24 +43,24 @@ function IndexPage() {
             Our primary goal to unite and help form Pokémon societies across the country, if you're looking to set a PokéSoc up, we can help you out!
             </p>
           </div>
-          <div id="home-logo-wrapper">
-            <img id="home-logo" src={ Doggo } link="" />
+          <div id={ styles.homeLogoWrapper }>
+            <img id={ styles.homeLogo } src={ Doggo } link="" />
           </div>
         </div>
 
         <VideoBanner videoSource={ LogoVideo } type="mp4" alt="Images of society logos moving as a collage."/>
         
-        <section className="index-double-section">
-          <div id="discord-section">
-            <img id="discord-logo" src={ DiscordIcon } alt="Discord logo."/>
-            <p id="discord-desc">Feel free to join if you are a member of a Pokémon Society.</p>
-            <RoundButton text="Join Our Discord" link="https://discord.gg/52YutzNUGg" dark={true} className="discord-button"/>
+        <section className={ styles.indexDoubleSection }>
+          <div id={ styles.discordSection }>
+            <img id={ styles.discordLogo } src={ DiscordIcon } alt="Discord logo."/>
+            <p id={ styles.discordDesc }>Feel free to join if you are a member of a Pokémon Society.</p>
+            <RoundButton text="Join Our Discord" link="https://discord.gg/52YutzNUGg" dark={true} className={ styles.discordButton }/>
           </div>
           
-          <div id="insta-section">
-            <img id="insta-logo" src={ InstaIcon } alt="Instagram logo."/>
-            <p id="insta-desc">Our many photos of Pokémon related things.</p>
-            <RoundButton text="View our Instagram" link="https://www.instagram.com/inter.uni.pokemon/" dark={true} className="insta-button"/>
+          <div id={ styles.instaSection }>
+            <img id={ styles.instaLogo } src={ InstaIcon } alt="Instagram logo."/>
+            <p id={ styles.instaDesc }>Our many photos of Pokémon related things.</p>
+            <RoundButton text="View our Instagram" link="https://www.instagram.com/inter.uni.pokemon/" dark={true} className={ styles.instaButton }/>
           </div>
         </section>
 
